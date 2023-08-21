10bb8048e0370402722696d040a56a494335c2c7
Erik Mejia <erikdojo352@gmail.com>
2023-08-20 20:42:56 -0400

    API fetch - MovieList

--- src/components/Header.js
+++ src/components/Header.js
@@ -178,7 +178,7 @@ export const Header = () => {
                 autoComplete="off"
               />
             </div>
-            <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+            <ul className="flex flex-col p-4 md:p-0 mt-4 font-normal border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
               <li>
                 <NavLink
                   to="/"
--- src/components/MovieListComponent.js
+++ src/components/MovieListComponent.js
@@ -1,11 +1,27 @@
+import { useEffect, useState } from "react";
 import { MovieCard } from "../components";
 
 export const MovieListComponent = () => {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    async function fetchMovies() {
+      const response = await fetch(
+        "https://api.themoviedb.org/3/movie/now_playing?api_key=6ab065a08b162fcedfff0b12d13dd9e4&language=en-US&page=1"
+      );
+      const data = await response.json();
+      setMovies(data.results);
+    }
+    fetchMovies();
+  }, []);
+
   return (
     <main>
       <section className="max-w-7xl mx-auto py-7">
         <div className="flex justify-start flex-wrap">
-          <MovieCard />
+          {movies.map((movie) => (
+            <MovieCard key={movie.id} movie={movie} />
+          ))}
         </div>
       </section>
     </main>
