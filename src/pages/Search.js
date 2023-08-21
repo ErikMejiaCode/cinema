4428d10202d7cac1c8296ce12a108e89b6e064bb
Erik Mejia <erikdojo352@gmail.com>
2023-08-21 17:19:13 -0400

    Movie search configuration

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,5 +1,28 @@
-import React from "react";
+import { useFetch } from "../hooks/useFetch";
+import { MovieCard } from "../components";
+import { useSearchParams } from "react-router-dom";
 
-export const Search = () => {
-  return <main>Search</main>;
+export const Search = ({ apiPath }) => {
+  const [searchParams] = useSearchParams();
+  const queryTerm = searchParams.get("q");
+  const { data: movies } = useFetch(apiPath, queryTerm);
+
+  return (
+    <main>
+      <section className="py-5">
+        <p className="text-3xl text-gray-700 dark:text-white">
+          {movies.length === 0
+            ? `No result found for '${queryTerm}'`
+            : `Results for '${queryTerm}'`}
+        </p>
+      </section>
+      <section className="max-w-7xl mx-auto py-7">
+        <div className="flex justify-start flex-wrap">
+          {movies?.map((movie) => (
+            <MovieCard key={movie.id} movie={movie} />
+          ))}
+        </div>
+      </section>
+    </main>
+  );
 };
